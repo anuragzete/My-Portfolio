67aafcf5b6e007983c2e509b7d0bf102402332aa
Anurag Zete <anuragzete27@outlook.com>
2025-07-05 22:29:31 +0530

    Fixed: Path issues
    Fixed the issues arised due to relative path and included the development env variables.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-import { ThemeProvider } from '../context/ThemeContext.jsx';
+import { ThemeProvider } from './context/ThemeContext.jsx';
 import App from './App.jsx';
-import '../index.css';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // Load environment variables based on the mode (development or production)
 export default defineConfig(({ mode }) => {
@@ -9,7 +10,16 @@ export default defineConfig(({ mode }) => {
   dotenv.config({ path: `.env.${mode}` });
 
   return {
-    plugins: [react()],
+    plugins: [react(),
+      VitePWA({
+        // only enable in production
+        devOptions: { enabled: false },
+        registerType: 'autoUpdate',
+        manifest: {
+          /* your manifest here */
+        }
+      })
+    ],
     optimizeDeps: {
       exclude: ['lucide-react'],
     },
